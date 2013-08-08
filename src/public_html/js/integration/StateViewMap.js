f65b29ab810f031ef7c70f3ffdac89c3a9530924
Michiel van der Ros <micros@mail.com>
2013-08-08 18:07:16 +0200

    Added passing unit tests for simple backbone example

---
+++ src/public_html/js/integration/StateViewMap.js
@@ -0,0 +1,19 @@
+this.navigatorjs = this.navigatorjs||{};
+this.navigatorjs.integration = this.navigatorjs.integration||{};
+
+(function() {
+
+	var StateViewMap = function (navigator) {
+
+	};
+
+	//PUBLIC API
+	StateViewMap.prototype = {
+		mapState: function(stateOrStates) {
+
+		}
+	};
+
+	navigatorjs.integration.StateViewMap = StateViewMap;
+
+}());
