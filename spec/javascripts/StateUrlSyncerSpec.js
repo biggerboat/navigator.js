43e418e28a66dbd3c9b2b0eb1aa81ca9fdafa3d6
Paul Tondeur <paul@paultondeur.nl>
2013-10-03 19:08:11 +0200

    Attempt to satisfy failing travis-ci. Somehow it doesn't like the delayedExpect's for pushState testing, while it works fine in local browsers. Tweaking delay to see if it makes any difference.

--- spec/javascripts/StateUrlSyncerSpec.js
+++ spec/javascripts/StateUrlSyncerSpec.js
@@ -116,8 +116,8 @@ describe("StateUrlSyncer", function() {
 				delayedExpect(function(){
 					expect(stateUrlSyncer.getRawUrl()).not.toEqual('test/');
 					expect(stateUrlSyncer.getRawUrl()).toEqual('');
-				},500);
-			},500);
+				},1000);
+			},1000);
 		});
 
 	});
