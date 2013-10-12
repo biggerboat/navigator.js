ae5c62f78f9a825797170d107b5bac937459546e
Paul Tondeur <paul@paultondeur.nl>
2013-10-13 00:17:00 +0200

    Improved the history spec descriptions and added a few extra tests

--- spec/javascripts/HistorySpec.js
+++ spec/javascripts/HistorySpec.js
@@ -11,10 +11,20 @@ describe('History', function() {
 		navigator.add({}, 'state2');
 		navigator.add({}, 'state3');
 		navigator.add({}, 'state4');
+		navigator.add({}, 'state5');
 		navigator.start('/');
 	});
 
 	it('starts with no history', function() {
+		navigator = new navigatorjs.Navigator();
+		history = new navigatorjs.History(navigator);
+
+		expect(history.getPreviousState()).toBeNull();
+		expect(history.getNextState()).toBeNull();
+		expect(history.getCurrentState()).toBeNull();
+
+		navigator.start('/');
+
 		expect(history.getPreviousState()).toBeNull();
 		expect(history.getNextState()).toBeNull();
 		expect(history.getCurrentState().getPath()).toEqual('/');
@@ -24,6 +34,15 @@ describe('History', function() {
 		navigator.request('state1');
 		expect(history.getPreviousState().getPath()).toEqual('/');
 		expect(history.getNextState()).toBeNull();
+	});
+
+	it('has one history state after start', function() {
+		navigator = new navigatorjs.Navigator();
+		history = new navigatorjs.History(navigator);
+
+		expect(history.getLength()).toEqual(0);
+		navigator.start('/');
+		expect(history.getLength()).toEqual(1);
 	});
 
 	it('has a history length that equals the number of state changes', function() {
@@ -37,37 +56,45 @@ describe('History', function() {
 		expect(history.getCurrentState().getPath()).toEqual('/');
 	});
 
-	it('does not allow navigation into future', function() {
+	it('does not allow to navigate forwards if we did not go back first', function() {
 		navigator.request('state1');
+
 		expect(history.forward()).toBe(false);
 		expect(history.getCurrentState().getPath()).toEqual('/state1/');
 	});
 
 	it('navigates back in history', function() {
 		navigator.request('state1');
+
+		expect(history.getCurrentState().getPath()).toEqual('/state1/');
 		expect(history.back()).toBe(true);
+
 		expect(history.getCurrentState().getPath()).toEqual('/');
 		expect(history.getNextState().getPath()).toEqual('/state1/');
 	});
 
-	it('navigates back in history with multiple items', function() {
+	it('navigates back in history when there are multiple history entries', function() {
 		navigator.request('state1');
 		navigator.request('state2');
+
+		expect(history.getCurrentState().getPath()).toEqual('/state2/');
 		expect(history.back()).toBe(true);
+
 		expect(history.getCurrentState().getPath()).toEqual('/state1/');
 		expect(history.getNextState().getPath()).toEqual('/state2/');
 		expect(history.getPreviousState().getPath()).toEqual('/');
 	});
 
-	it('navigates back multiple times', function() {
+	it('navigates back multiple to multiple history entries', function() {
 		navigator.request('state1');
 		navigator.request('state2');
+
 		expect(history.back()).toBe(true);
 		expect(history.back()).toBe(true);
 		expect(history.getCurrentState().getPath()).toEqual('/');
 	});
 
-	it('navigates back multiple steps', function() {
+	it('navigates back with multiple steps at once', function() {
 		navigator.request('state1');
 		navigator.request('state2');
 		expect(history.back(2)).toBe(true);
@@ -81,7 +108,7 @@ describe('History', function() {
 		expect(history.getCurrentState().getPath()).toEqual('/state1/');
 	});
 
-	it('navigates forward multiple times', function() {
+	it('navigates forward across multiple history items', function() {
 		navigator.request('state1');
 		navigator.request('state2');
 		expect(history.back()).toBe(true);
@@ -91,7 +118,7 @@ describe('History', function() {
 		expect(history.getCurrentState().getPath()).toEqual('/state2/');
 	});
 
-	it('navigates forward multiple steps in history', function() {
+	it('navigates forward with multiple steps at once', function() {
 		navigator.request('state1');
 		navigator.request('state2');
 		expect(history.back()).toBe(true);
@@ -100,14 +127,14 @@ describe('History', function() {
 		expect(history.getCurrentState().getPath()).toEqual('/state2/');
 	});
 
-	it('navigates backwards multiple steps within bounds', function() {
+	it('navigates backwards multiple steps but caps it within history bounds', function() {
 		navigator.request('state1');
 		navigator.request('state2');
 		expect(history.back(10)).toBe(true);
 		expect(history.getCurrentState().getPath()).toEqual('/');
 	});
 
-	it('navigates forward multiple steps within bounds', function() {
+	it('navigates forward multiple steps but caps it within history bounds', function() {
 		navigator.request('state1');
 		navigator.request('state2');
 		expect(history.back()).toBe(true);
@@ -116,48 +143,49 @@ describe('History', function() {
 		expect(history.getCurrentState().getPath()).toEqual('/state2/');
 	});
 
-	it('retrieve the previous NavigationState', function() {
+	it('retrieves the previous NavigationState', function() {
 		navigator.request('state1');
 		navigator.request('state2');
 		expect(history.getPreviousState().getPath()).toEqual('/state1/');
 	});
 
-	it('retrieve the previous NavigationState with multiple steps', function() {
+	it('retrieves the previous NavigationState with multiple steps', function() {
 		navigator.request('state1');
 		navigator.request('state2');
 		navigator.request('state3');
 		expect(history.getPreviousState(2).getPath()).toEqual('/state1/');
 	});
 
-	it('retrieve the previous NavigationState with multiple steps within bounds', function() {
+	it('retrieves the previous NavigationState with multiple steps within bounds', function() {
 		navigator.request('state1');
 		navigator.request('state2');
 		navigator.request('state3');
 		expect(history.getPreviousState(100).getPath()).toEqual('/');
 	});
 
-	it('retrieve the next NavigationState in future returns null', function() {
+	it('retrieves null when the next state is requested while there is no future', function() {
 		navigator.request('state1');
 		navigator.request('state2');
 		expect(history.getNextState()).toBeNull();
+		expect(history.getNextState(10)).toBeNull();
 	});
 
-	it('retrieve the next NavigationState', function() {
+	it('retrieves the next NavigationState when there is a future', function() {
 		navigator.request('state1');
 		navigator.request('state2');
 		history.back();
 		expect(history.getNextState().getPath()).toEqual('/state2/');
 	});
 
-	it('retrieve the next NavigationState with multiple steps', function() {
+	it('retrieves the next NavigationState with multiple steps at once', function() {
 		navigator.request('state1');
 		navigator.request('state2');
 		navigator.request('state3');
-		history.back(10);
+		history.back(3);
 		expect(history.getNextState(2).getPath()).toEqual('/state2/');
 	});
 
-	it('retrieve the next NavigationState with multiple steps within bounds', function() {
+	it('retrieves the next NavigationState with multiple steps at once capped within history bounds', function() {
 		navigator.request('state1');
 		navigator.request('state2');
 		navigator.request('state3');
@@ -182,25 +210,40 @@ describe('History', function() {
 		expect(history.getPreviousState()).toBeNull();
 	});
 
-	it('state by position matches', function() {
+	it('has no current state after the history is cleared', function() {
+		navigator.request('state1');
+		navigator.request('state2');
+		navigator.request('state3');
+		history.clearHistory();
+		expect(history.getCurrentState()).toBeNull();
+	});
+
+	it('can access states by absolute position in the history', function() {
 		navigator.request('state1');
 		expect(history.getStateByPosition(1).getPath()).toEqual('/');
 		expect(history.getStateByPosition(0).getPath()).toEqual('/state1/');
 	});
 
-	it('position by path matches', function() {
+	it('can retrieve the history position by path', function() {
 		navigator.request('state1');
 		expect(history.getPositionByPath('/state1/')).toEqual(0);
 		expect(history.getPositionByPath('/')).toEqual(1);
 	});
 
-	it('position by state matches', function() {
+	it('returns the index of the first history item that is matched', function() {
+		navigator.request('state1');
+		navigator.request('state2');
+		navigator.request('state1');
+		expect(history.getPositionByPath('/state1/')).toEqual(0);
+	});
+
+	it('can retrieve the history position by state', function() {
 		navigator.request('state1');
 		expect(history.getPositionByState(history.getCurrentState())).toEqual(0);
 		expect(history.getPositionByState(history.getPreviousState())).toEqual(1);
 	});
 
-	it('max length limit works', function() {
+	it("keeps the history length within defined bounds and removes older states from the history", function() {
 		history.maxLength = 3;
 		navigator.request('state1');
 		navigator.request('state2');
@@ -209,7 +252,36 @@ describe('History', function() {
 		navigator.request('state5');
 
 		expect(history.getLength()).toEqual(3);
-		expect(history.getStateByPosition(history.getLength() - 1).getPath()).toEqual('/state3/');
+		expect(history.getStateByPosition(0).getPath()).toEqual('/state5/');
+		expect(history.getStateByPosition(2).getPath()).toEqual('/state3/');
+	});
+
+	it("clears the future when we go back and navigate to a new state", function() {
+		navigator.request('state1');
+		navigator.request('state2');
+		navigator.request('state3');
+		history.back(3);
+
+		expect(history.getLength()).toEqual(4);
+		navigator.request('state4');
+		expect(history.getLength()).toEqual(2);
+
+		expect(history.getPreviousState().getPath()).toEqual('/');
+		expect(history.getNextState()).toBeNull();
+	});
+
+	it("doesn't clear the future when we go back and forward", function() {
+		navigator.request('state1');
+		navigator.request('state2');
+		navigator.request('state3');
+
+		history.back(3);
+		expect(history.getLength()).toEqual(4);
+
+		history.forward(2);
+		expect(history.getLength()).toEqual(4);
+
+		expect(history.getCurrentState().getPath()).toEqual('/state2/');
 	});
 
 });
