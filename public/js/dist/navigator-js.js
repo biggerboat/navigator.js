7b67c763b833c996e730ca657a60b0bb7c1be632
Paul Tondeur <paul@paultondeur.nl>
2013-10-28 09:19:59 +0100

    Release v0.4.1

--- public/js/dist/navigator-js.js
+++ public/js/dist/navigator-js.js
@@ -576,9 +576,12 @@ this.navigatorjs.NavigationResponderBehaviors.getInterfaceMethods = function(int
 				nativeMatch = foreignState.getPath().match(this.getPathRegex()),
 				isForeignMatch = foreignMatch && foreignMatch.index == 0 ? true : false,
 				isNativeMatch = nativeMatch && nativeMatch.index == 0 ? true : false,
-				tooManyForeignSegments = foreignSegments.length > nativeSegments.length;
+				foreignSegmentDoubleWildcardsMatch = foreignState.getPath().match(/\*\*/g),
+				doubleWildcardsLength = foreignSegmentDoubleWildcardsMatch ? foreignSegmentDoubleWildcardsMatch.length : 0,
+				tooManyForeignSegments = foreignSegments.length > (nativeSegments.length + doubleWildcardsLength),
+				enoughNativeSegments = nativeSegments.length > foreignSegments.length;
 
-			return (isForeignMatch || isNativeMatch) && !tooManyForeignSegments;
+			return (isForeignMatch || (isNativeMatch && enoughNativeSegments)) && !tooManyForeignSegments;
 		},
 
 		_containsStateInArray: function(foreignStatesOrPaths) {
@@ -632,7 +635,7 @@ this.navigatorjs.NavigationResponderBehaviors.getInterfaceMethods = function(int
 			if (!this.contains(operand)) {
 				return null;
 			}
-
+			
 			subtractedPath = this.getPath().replace(operand.getPathRegex(), "");
 
 			return new navigatorjs.NavigationState(subtractedPath);
